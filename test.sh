509bf7d5b8f612ecb0e6bc61fb792776e6ea26d7
bfs15 <bruno.serbena@gmail.com>
2018-05-20 12:09:38 -0300

    test scripts now print results after execution

--- test.sh
+++ test.sh
@@ -3,4 +3,6 @@ echo > test.log;
 for FILE in test/*.tig; do
      ./parser.exe $FILE || echo $FILE failed >> test.log
 done
+echo "Test Failures:"
+cat test.log
 
--- testError.sh
+++ testError.sh
@@ -6,5 +6,13 @@ for DIR in test/error/* ; do
 	for FILE in $DIR/*.tig; do
 		./parser.exe $FILE && echo $FILE failed >> $DIR/testError.log
 	done
+	
+	echo "Test Failures " $DIR ": " >> testError.log
+	cat $DIR/testError.log >> testError.log
+	echo >> testError.log
 done
+
+echo "Test Failures:"
+cat testError.log
+
 
